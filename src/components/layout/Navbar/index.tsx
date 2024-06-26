618c203b39d4f95d36a1496284c00b11cd566536
ReylanLugo <ClockR13@gmail.com>
2024-06-26 06:36:17 -0400

    feat: add navbar routes

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,32 +1,56 @@
 import React from "react";
 import Image from "next/image";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
-export const Navbar: React.FC = () => {
+export const Navbar: React.FC = async () => {
+  const session = await createClient().auth.getUser();
+
   return (
     <>
       <div
         className={
-          "flex w-full justify-between bg-white px-8 py-4 shadow shadow-slate-300"
+          "flex w-full justify-between border-b border-slate-200 bg-white px-8 py-4 shadow shadow-slate-300"
         }
       >
         <Image src={"/logo.png"} alt={"logo"} width={64} height={64} />
         <nav className={"flex items-center gap-3 text-sm"}>
-          <a className={"text-slate-500 hover:text-slate-700"} href={"#"}>
-            Home
-          </a>
-          <a className={"text-slate-500 hover:text-slate-700"} href={"#"}>
-            Favorites
-          </a>
-          <a className={"text-slate-500 hover:text-slate-700"} href={"#"}>
-            My Apartments
-          </a>
-          <Image
-            src={"/defaultProfileImg.png"}
-            alt={"profile"}
-            width={40}
-            height={40}
-            className={"cursor-pointer rounded-full"}
-          />
+          {session ? (
+            <>
+              <Link
+                href={"/"}
+                className={"text-slate-500 hover:text-slate-700"}
+              >
+                Home
+              </Link>
+              <Link
+                href={"/favorites"}
+                className={"text-slate-500 hover:text-slate-700"}
+              >
+                Favorites
+              </Link>
+              <Link
+                href={"/myapartments"}
+                className={"text-slate-500 hover:text-slate-700"}
+              >
+                My Apartments
+              </Link>
+              <Image
+                src={"/defaultProfileImg.png"}
+                alt={"profile"}
+                width={40}
+                height={40}
+                className={"cursor-pointer rounded-full"}
+              />
+            </>
+          ) : (
+            <a
+              href={"/login"}
+              className={"text-slate-500 hover:text-slate-700"}
+            >
+              Login
+            </a>
+          )}
         </nav>
       </div>
     </>
