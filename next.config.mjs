59ece990a7666ce1ac5ad06ba759782d2cb53f2e
ReylanLugo <ClockR13@gmail.com>
2024-06-26 04:39:38 -0400

    fix: add bucket url in nextjs urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "localhost" },
+      { hostname: "yibpwvdelepvsdlvwaia.supabase.co" },
+    ],
+  },
+};
 
 export default nextConfig;
